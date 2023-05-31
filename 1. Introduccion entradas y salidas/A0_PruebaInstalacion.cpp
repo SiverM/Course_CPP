ffec62bfefe16e8937866056d6ad213a1093b8ea
Iver Medina <ivermedina55@gmail.com>
2023-05-31 19:02:27 -0400

    agregando primeros ejercicios

---
+++ 1. Introduccion entradas y salidas/A0_PruebaInstalacion.cpp
@@ -0,0 +1,8 @@
+// primer programa en C++
+#include<iostream>
+using namespace	std;
+int main(){
+	cout<<"Hola mundo :D"<<endl;
+	return 0;
+}
+
---
+++ 1. Introduccion entradas y salidas/A1_TiposDeDatosBasicosEnCpp.cpp
@@ -0,0 +1,14 @@
+//Tipos de datos básicos en C++
+#include<iostream>
+using namespace std;
+int main(){
+	int entero=15;
+	cout<<entero<<endl;
+	float flotante=10.45;
+	cout<<flotante<<endl;
+	double mayor=16.3456;
+	cout<<mayor<<endl;
+	char letra='a';
+	cout<<letra<<endl;
+	return 0;
+}
